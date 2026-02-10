d29c5e28c24e3f958934ee2c6de3abed6723c357
MrXnneHang <XnneHang@gmail.com>
2026-02-10 09:45:57 +0800

    dx: set default logger level to ERROR

--- src/gsv/GPT_SoVITS/utils.py
+++ src/gsv/GPT_SoVITS/utils.py
@@ -15,7 +15,7 @@ import logging
 
 logging.getLogger("numba").setLevel(logging.ERROR)
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
 logger = logging
 
 
